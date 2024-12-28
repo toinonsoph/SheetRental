8c58799f252666aaf2f2e6334123453eae924f96
Toinon Naesen <toinon.naesen@axi.be>
2024-12-28 22:31:02 +0100

    Added GitHub Actions build workflow

--- angular-frontend/src/environments/environment.ts
+++ angular-frontend/src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
     production: false,
-    supabaseUrl: 'https://uykfaveopnggzehxzmds.supabase.co',
-    supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InV5a2ZhdmVvcG5nZ3plaHh6bWRzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzQwMzMzNDgsImV4cCI6MjA0OTYwOTM0OH0.bn-AK_iGvwcgOGHn0kiX8J6xlMA0sdDGdoV7k7EsSyE',
+    supabaseUrl: process.env.SUPABASE_URL || '',
+    supabaseKey: process.env.SUPABASE_KEY || '',
     sendgridApiKey: '',
     supabaseStorage: {
       bucket: 'villas_and_apartments',
