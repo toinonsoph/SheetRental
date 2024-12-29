49a141dca04dba47123accd76b3cd452d8c7fbbd
Toinon Naesen <toinon.naesen@axi.be>
2024-12-29 17:14:26 +0100

    Changes for keys

--- angular-frontend/src/environments/environment.ts
+++ angular-frontend/src/environments/environment.ts
@@ -1,10 +1,10 @@
 export const environment = {
-    production: false,
-    supabaseUrl: process.env.SUPABASE_URL || '',
-    supabaseKey: process.env.SUPABASE_KEY || '',
-    sendgridApiKey: '',
-    supabaseStorage: {
-      bucket: 'villas_and_apartments',
-      iconsBucket: 'icons'
-    }
-  };
+  production: false,
+  supabaseUrl: process.env.SUPABASE_URL || '',
+  supabaseKey: process.env.SUPABASE_KEY || '',
+  sendgridApiKey: process.env.SENDGRID_API_KEY || '',
+  supabaseStorage: {
+    bucket: 'villas_and_apartments',
+    iconsBucket: 'icons'
+  }
+};
