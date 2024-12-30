4e7928db1ceb8d3737e3e6e4159d159a025fdc4e
Toinon Naesen <toinon.naesen@axi.be>
2024-12-30 20:22:27 +0100

    Revert "Changes for nojekyll"
    This reverts commit f60b14a5cab21a384abdbf2e1a59f6ce157d8847.

--- angular-frontend/src/app/components/verhuur/verhuur.component.ts
+++ angular-frontend/src/app/components/verhuur/verhuur.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common'; 
+import { CommonModule } from '@angular/common'; // Import CommonModule for *ngFor
 
 import { SupabaseService } from '../../supabase.service';
 import { environment } from '../../../environments/environment';
