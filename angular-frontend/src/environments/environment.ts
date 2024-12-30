9a380527292760cf3c317d9cf145db7b32c7ab63
Toinon Naesen <toinon.naesen@axi.be>
2024-12-30 23:04:45 +0100

    Adding supabase

--- angular-frontend/src/environments/environment.ts
+++ angular-frontend/src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: false,
-  supabaseUrl: '__SUPABASE_URL__',
-  supabaseKey: '__SUPABASE_KEY__',
+  supabaseUrl: 'https://uykfaveopnggzehxzmds.supabase.co',
+  supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InV5a2ZhdmVvcG5nZ3plaHh6bWRzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzQwMzMzNDgsImV4cCI6MjA0OTYwOTM0OH0.bn-AK_iGvwcgOGHn0kiX8J6xlMA0sdDGdoV7k7EsSyE',
   supabaseStorage: {
     bucket: 'villas_and_apartments',
     iconsBucket: 'icons'
