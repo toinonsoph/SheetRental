88ae41442557ae9b27cca450674a4700a16886e8
Toinon Naesen <toinon.naesen@axi.be>
2024-12-30 23:19:22 +0100

    Renaming iconsBucket to iconBucket

--- angular-frontend/src/environments/environment.ts
+++ angular-frontend/src/environments/environment.ts
@@ -4,6 +4,6 @@ export const environment = {
   supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InV5a2ZhdmVvcG5nZ3plaHh6bWRzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzQwMzMzNDgsImV4cCI6MjA0OTYwOTM0OH0.bn-AK_iGvwcgOGHn0kiX8J6xlMA0sdDGdoV7k7EsSyE',
   supabaseStorage: {
     bucket: 'villas_and_apartments',
-    iconsBucket: 'icons'
+    iconBucket: 'icons'
   }
 };
