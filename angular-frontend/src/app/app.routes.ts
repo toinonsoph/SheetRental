1f97bb6aa37ad58d85d99366683d2e65a707a2ec
Toinon Naesen <toinon.naesen@axi.be>
2025-01-01 12:41:30 +0100

    Revert "Changes to routing"
    This reverts commit 07abcd6d8f7c9a89faa5ab11f698ad3e4af3e7c9.

--- angular-frontend/src/app/app.routes.ts
+++ angular-frontend/src/app/app.routes.ts
@@ -6,11 +6,11 @@ import { VerhuurComponent } from './components/verhuur/verhuur.component';
 import { LakensComponent } from './components/lakens/lakens.component';
 
 export const routes: Routes = [
-  { path: '/login', component: LoginComponent }, 
-  { path: '/user', component: UserComponent, canActivate: [AuthGuard] }, 
+  { path: 'login', component: LoginComponent }, 
+  { path: 'user', component: UserComponent, canActivate: [AuthGuard] }, 
 
-  { path: '/rental', component: VerhuurComponent }, 
-  { path: '/sheets', component: LakensComponent }, 
+  { path: 'rental', component: VerhuurComponent }, 
+  { path: 'sheets', component: LakensComponent }, 
 
   { path: '', redirectTo: '/sheets', pathMatch: 'full' }, 
   { path: '**', redirectTo: '/sheets' }, 
