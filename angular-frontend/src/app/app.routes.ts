055ec28f967d52dcaa432c663f1677c79bbb4484
Toinon Naesen <toinon.naesen@axi.be>
2025-01-01 11:48:51 +0100

    Changes to have a login part

--- angular-frontend/src/app/app.routes.ts
+++ angular-frontend/src/app/app.routes.ts
@@ -2,24 +2,18 @@ import { Routes } from '@angular/router';
 import { UserComponent } from './components/user/user.component';
 import { LoginComponent } from './components/login/login.component';
 import { AuthGuard } from './guards/auth.guard';
+import { VerhuurComponent } from './components/verhuur/verhuur.component';
+import { LakensComponent } from './components/lakens/lakens.component';
 
 export const routes: Routes = [
-  {
-    path: 'login',
-    component: LoginComponent, 
-  },
-  {
-    path: 'user',
-    component: UserComponent, 
-    canActivate: [AuthGuard], 
-  },
-  {
-    path: '',
-    redirectTo: '/login', 
-    pathMatch: 'full',
-  },
-  {
-    path: '**',
-    redirectTo: '/login', 
-  },
+  { path: 'login', component: LoginComponent }, 
+  { path: 'user', component: UserComponent, canActivate: [AuthGuard] }, 
+
+  // Unprotected routes
+  { path: 'verhuur', component: VerhuurComponent }, 
+  { path: 'lakens', component: LakensComponent }, 
+
+  // Default and fallback routes
+  { path: '', redirectTo: '/lakens', pathMatch: 'full' }, 
+  { path: '**', redirectTo: '/lakens' }, 
 ];
