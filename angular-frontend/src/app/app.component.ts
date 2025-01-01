68043f224c511123f5b93dc0ec9f213f846fcea8
Toinon Naesen <toinon.naesen@axi.be>
2025-01-01 12:51:50 +0100

    Routes for rental part and sheet part

--- angular-frontend/src/app/app.component.ts
+++ angular-frontend/src/app/app.component.ts
@@ -23,8 +23,8 @@ import { AdditionalInfoComponent } from './components/additional-info/additional
 })
 export class AppComponent {
   links = [
-    { id: 'sheets', label: 'Lakens / Draps / Bettwâsche' },
-    { id: 'rental', label: 'Vakantiewoningen / Location de maisons de vacances / Vermietung von Ferienhäuser' },
+    { id: '/sheets', label: 'Lakens / Draps / Bettwâsche' },
+    { id: '/rental', label: 'Vakantiewoningen / Location de maisons de vacances / Vermietung von Ferienhäuser' },
   ];
 
   activeLink = this.links[0].id;
