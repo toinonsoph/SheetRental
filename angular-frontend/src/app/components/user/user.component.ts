242131e7448872601d45d2b29d5a245f4258923f
Toinon Naesen <toinon.naesen@axi.be>
2025-01-02 20:56:27 +0100

    Adding logout to the user component

--- angular-frontend/src/app/components/user/user.component.ts
+++ angular-frontend/src/app/components/user/user.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { AgenceCambreComponent } from '../agence-cambre/agence-cambre.component';
 import { CambreServicesComponent } from '../cambre-services/cambre-services.component';
 import { ChangeDetectorRef } from '@angular/core';
+import { Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-user',
@@ -18,7 +20,11 @@ export class UserComponent {
   message: string | null = '';
   selectedTab: string = 'services';
 
-  constructor(private cdr: ChangeDetectorRef) {}
+  constructor(
+    private cdr: ChangeDetectorRef,
+    private http: HttpClient,
+    private router: Router 
+  ) {}
 
   selectTab(tab: string) {
     this.selectedTab = tab;
@@ -27,5 +33,21 @@ export class UserComponent {
 
   clearMessage() {
     this.message = null;
+  }
+
+  logout() {
+    localStorage.removeItem('authToken');
+    sessionStorage.removeItem('authToken');
+    
+    this.http.post('/api/logout', {}).subscribe(
+      () => {
+        console.log('Session invalidated on server');
+      },
+      (error) => {
+        console.error('Error during logout request:', error);
+      }
+    );
+  
+    this.router.navigate(['/login']);
   }
 }
