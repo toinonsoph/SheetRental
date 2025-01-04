6e66ca2b243c7e0479d951e5b0acd09da9342fd6
Toinon Naesen <toinon.naesen@axi.be>
2025-01-04 13:11:01 +0100

    Changes to styling + spinner

--- angular-frontend/src/app/components/lakens/lakens.component.ts
+++ angular-frontend/src/app/components/lakens/lakens.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
@@ -35,28 +35,32 @@ export class LakensComponent implements OnInit {
 
   constructor(
     private supabaseService: SupabaseService,
-    private sendGridService: SendGridService
+    private sendGridService: SendGridService,
+    private cdr: ChangeDetectorRef
   ) {}
 
   async ngOnInit(): Promise<void> {
     try {
+      this.toggleSpinner(true); 
       const { data, error } = await this.supabaseService.client
         .from('material')
         .select('name_dutch, name_french, name_german, information_dutch, information_french, information_german, price');
-
+  
       if (error) {
         console.error('Error fetching data from Supabase:', error);
         return;
       }
-
+  
       this.cards = data || [];
       this.cards.forEach((card) => {
         this.quantities[card.name_dutch] = 0;
       });
     } catch (err) {
       console.error('Unexpected error:', err);
+    } finally {
+      this.toggleSpinner(false); 
     }
-  }
+  } 
 
   openPopup() {
     this.isPopupOpen = true;
@@ -66,7 +70,7 @@ export class LakensComponent implements OnInit {
 
   closePopup() {
     this.isPopupOpen = false;
-    this.isLoading = false;
+    this.toggleSpinner(false);
     this.resetForm();
   }
 
@@ -74,13 +78,13 @@ export class LakensComponent implements OnInit {
     this.formSubmitted = true;
   
     if (this.isFormValid()) {
-      this.isLoading = true;
+      this.toggleSpinner(true); 
       const templateIdRequest = 'd-b4eb9feefbde4aa4a3ead2460c8c973d';
       const templateIdReceived = 'd-89a05b23f6b146c4a2243e7c485e2260';
       const dynamicData: any = {
         first_name: this.formData.firstName,
         last_name: this.formData.lastName,
-        email: this.formData.mail, // User's email
+        email: this.formData.mail,
         phone_number: this.formData.phone,
         remark: this.formData.remark,
       };
@@ -90,41 +94,35 @@ export class LakensComponent implements OnInit {
         dynamicData[`quantity_${cardKey}`] = (this.quantities[card.name_dutch] || 0).toString();
       });
   
-      this.successMessage = null;
-      this.errorMessage = null;
-  
-      this.sendGridService
-        .sendEmailWithTemplate('tnsn@axi.be', templateIdRequest, dynamicData)
-        .subscribe({
-          next: () => {  
-            this.sendGridService
-              .sendEmailWithTemplateWithoutDynamicData(this.formData.mail, templateIdReceived) 
-              .subscribe({
-                next: () => {
-                  this.successMessage = 
-                    'E-mail is succesvol verzonden! Check je e-mail voor een verzendbevestiging. Check indien nodig de spam folder.<br>' +
-                    'L\'e-mail a été envoyé avec succès! Vérifiez votre courrier électronique pour une confirmation d\'expédition. Vérifiez le dossier spam si nécessaire.<br>' +
-                    'E-Mail wurde erfolgreich gesendet! Überprüfen Sie Ihre E-Mail auf eine Versandbestätigung. Überprüfen Sie ggf. den Spam-Ordner.';
-                  this.resetForm(); 
-                  this.isLoading = false;
-                },
-                error: () => {
-                  this.errorMessage = 'The first email was sent to Cambre Services, but there was an issue with sending a received message to you. Please try again.';
-                  this.isLoading = false;
-                },
-              });
-          },
-          error: () => {            
-            this.errorMessage = 'There was an issue with sending the email. Please try again.';
-            this.isLoading = false;
-          },
-        });
+      this.sendGridService.sendEmailWithTemplate('tnsn@axi.be', templateIdRequest, dynamicData).subscribe({
+        next: () => {
+          this.sendGridService.sendEmailWithTemplateWithoutDynamicData(this.formData.mail, templateIdReceived).subscribe({
+            next: () => {
+              this.successMessage =
+                'E-mail is succesvol verzonden! Check je e-mail voor een verzendbevestiging. Check indien nodig de spam folder.<br>' +
+                "L'e-mail a été envoyé avec succès! Vérifiez votre courrier électronique pour une confirmation d'expédition. Vérifiez le dossier spam si nécessaire.<br>" +
+                'E-Mail wurde erfolgreich gesendet! Überprüfen Sie Ihre E-Mail auf eine Versandbestätigung. Überprüfen Sie ggf. den Spam-Ordner.';
+              this.resetForm();
+              this.toggleSpinner(false); 
+            },
+            error: () => {
+              this.errorMessage =
+                'The first email was sent to Cambre Services, but there was an issue with sending a received message to you. Please try again.';
+              this.toggleSpinner(false); 
+            },
+          });
+        },
+        error: () => {
+          this.errorMessage = 'There was an issue with sending the email. Please try again.';
+          this.toggleSpinner(false); 
+        },
+      });
     } else {
       this.errorMessage = 'The form is not valid. Please fill in all required fields.';
-      this.isLoading = false;
+      this.toggleSpinner(false); 
       this.isPopupOpen = true;
     }
-  }  
+  } 
 
   resetForm() {
     this.formSubmitted = false;
@@ -156,5 +154,11 @@ export class LakensComponent implements OnInit {
       isEmailValid &&
       isPhoneValid
     );
+  }
+
+  toggleSpinner(status: boolean) {
+    this.isLoading = status;
+    console.log(`Spinner status: ${status}`);
+    this.cdr.detectChanges();
   }
 }
