70ca6dab5721afed266ebce257c7c3ef8000d70d
Toinon Naesen <toinon.naesen@axi.be>
2025-01-04 22:47:48 +0100

    Styling changes

--- angular-frontend/src/app/app.routes.ts
+++ angular-frontend/src/app/app.routes.ts
@@ -4,10 +4,12 @@ import { LoginComponent } from './components/login/login.component';
 import { AuthGuard } from './guards/auth.guard';
 import { VerhuurComponent } from './components/verhuur/verhuur.component';
 import { LakensComponent } from './components/lakens/lakens.component';
+import { CambreServicesComponent } from './components/cambre-services/cambre-services.component';
 
 export const routes: Routes = [
   { path: 'login', component: LoginComponent }, 
   { path: 'user', component: UserComponent, canActivate: [AuthGuard] }, 
+  { path: 'cambre-services', component: CambreServicesComponent },
 
   { path: 'rental', component: VerhuurComponent }, 
   { path: 'sheets', component: LakensComponent }, 
