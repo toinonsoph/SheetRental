c8adfaaf902c9cb2f6a1768faad6f0411a2a4901
Toinon Naesen <toinon.naesen@axi.be>
2025-01-04 22:14:15 +0100

    Auto-generated commit

---
+++ chunk-R5HC3FKR.js
@@ -0,0 +1,1 @@
+import{f as t}from"./chunk-TWZW5B45.js";var o=t((r,e)=>{e.exports=function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")}});export default o();
