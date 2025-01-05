6c2cb65243128b2c4b076b4744ceb06b9671d078
Toinon Naesen <toinon.naesen@axi.be>
2025-01-05 10:57:24 +0100

    Adding agence cambre component

--- angular-frontend/src/app/components/agence-cambre/agence-cambre.component.ts
+++ angular-frontend/src/app/components/agence-cambre/agence-cambre.component.ts
@@ -1,5 +1,7 @@
 import { Component, Input } from '@angular/core';
 import { MatTabsModule } from '@angular/material/tabs';
+import { SupabaseService } from '../../services/supabase.service';
+import { MatTableModule } from '@angular/material/table';
 
 @Component({
   selector: 'app-agence-cambre',
@@ -7,10 +9,30 @@ import { MatTabsModule } from '@angular/material/tabs';
   styleUrls: ['./agence-cambre.component.css'],
   imports: 
   [
-      MatTabsModule
+      MatTabsModule,
+      MatTableModule 
   ],
   standalone: true
 })
 export class AgenceCambreComponent {
   @Input() selectedTab!: string;
+
+  houses: any[] = [];
+  equipments: any[] = [];
+  
+  displayedColumns: string[] = ['name', 'type', 'totalpersons', 'totalrooms', 'price', 'url'];
+  equipmentColumns: string[] = ['name', 'image'];
+
+  constructor(private supabaseService: SupabaseService) {}
+
+  async onTabChange(event: any) {
+    const selectedIndex = event.index;
+
+    if (selectedIndex === 0 && this.houses.length === 0) {
+      this.houses = await this.supabaseService.fetchHousesWithAddresses();
+    } else if (selectedIndex === 1 && this.equipments.length === 0) {
+      await this.supabaseService.fetchEquipments();
+      this.equipments = this.supabaseService.equipments;
+    }
+  }
 }
