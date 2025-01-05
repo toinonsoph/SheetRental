623a11fd61612367ea31fb833d0bbc9b111dd13e
Toinon Naesen <toinon.naesen@axi.be>
2025-01-05 22:37:56 +0100

    Showing agence cambre part

--- angular-frontend/src/app/components/user/user.component.ts
+++ angular-frontend/src/app/components/user/user.component.ts
@@ -18,6 +18,8 @@ import { AgenceCambreComponent } from '../agence-cambre/agence-cambre.component'
 })
 export class UserComponent {
   message: string | null = ''; 
+  showCambreServices: boolean = true; 
+  showAgenceCambre: boolean = false; 
 
   constructor(
     private http: HttpClient
@@ -43,5 +45,16 @@ export class UserComponent {
         window.location.href = '/login';
       },
     });
+  }
+
+  onTabChange(event: any) {
+    const selectedTabIndex = event.index;
+    if (selectedTabIndex === 0) {
+      this.showCambreServices = true;
+      this.showAgenceCambre = false;
+    } else if (selectedTabIndex === 1) {
+      this.showCambreServices = false;
+      this.showAgenceCambre = true;
+    }
   }
 }
