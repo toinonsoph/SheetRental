82b447c8686d67e2b4019f62f6e390122ddee1eb
Toinon Naesen <toinon.naesen@axi.be>
2025-01-05 14:50:34 +0100

    Adding imports

--- angular-frontend/src/app/components/confirm-dialog/confirm-dialog.component.ts
+++ angular-frontend/src/app/components/confirm-dialog/confirm-dialog.component.ts
@@ -1,10 +1,20 @@
 import { Component, Inject } from '@angular/core';
-import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { MatButtonModule } from '@angular/material/button';
+import { MAT_DIALOG_DATA, MatDialogModule, MatDialogRef } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-confirm-dialog',
   templateUrl: './confirm-dialog.component.html',
-  styleUrls: ['./confirm-dialog.component.css']
+  styleUrls: ['./confirm-dialog.component.css'],
+  imports: [
+      MatDialogModule,
+      MatButtonModule,
+      MatInputModule,
+      MatFormFieldModule 
+    ],
+    standalone: true
 })
 export class ConfirmDialogComponent {
   constructor(
