3961384ba3e54b3951f2377ff3a2870f774ec796
Toinon Naesen <toinon.naesen@axi.be>
2025-01-05 14:45:35 +0100

    Changes

--- angular-frontend/src/app/components/confirm-dialog/confirm-dialog.component.ts
+++ angular-frontend/src/app/components/confirm-dialog/confirm-dialog.component.ts
@@ -4,7 +4,7 @@ import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 @Component({
   selector: 'app-confirm-dialog',
   templateUrl: './confirm-dialog.component.html',
-  styleurls: ['./confirm-dialog.component.css']
+  styleUrls: ['./confirm-dialog.component.css']
 })
 export class ConfirmDialogComponent {
   constructor(
