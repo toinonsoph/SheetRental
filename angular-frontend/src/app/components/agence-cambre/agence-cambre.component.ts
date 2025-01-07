3040111e27e725bb871a34e956874798b84906d4
Toinon Naesen <toinon.naesen@axi.be>
2025-01-07 18:54:14 +0100

    Changes to navigation part 3

--- angular-frontend/src/app/components/agence-cambre/agence-cambre.component.ts
+++ angular-frontend/src/app/components/agence-cambre/agence-cambre.component.ts
@@ -21,15 +21,15 @@ export class AgenceCambreComponent implements OnInit {
     ngOnInit(): void {
       this.router.events.subscribe(() => {
         const url = this.router.url;
-        if (url === '/properties') {
+        if (url.includes('/user/agence-cambre/properties')) {
           this.activeLinkIndex = 0;
-        } else if (url === '/equipments') {
+        } else if (url.includes('/user/agence-cambre/equipments')) {
           this.activeLinkIndex = 1;
         } else {
           this.activeLinkIndex = -1; 
         }
       });
-    }
+    }    
   
     navigateTo(route: string): void {
       this.router.navigate([route], { relativeTo: this.router.routerState.root.firstChild?.children?.find(c => c.routeConfig?.path === 'agence-cambre') });
