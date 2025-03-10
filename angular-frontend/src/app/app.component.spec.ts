1ee944671bf5a9fb5883d8586d90e8fdccf5b253
Toinon Naesen <toinon.naesen@gmail.com>
2025-03-10 22:12:08 +0100

    Change to test that's not used

--- angular-frontend/src/app/app.component.spec.ts
+++ angular-frontend/src/app/app.component.spec.ts
@@ -14,11 +14,11 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'LakenVerHuur' title`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('LakenVerHuur');
-  });
+  // it(`should have the 'LakenVerHuur' title`, () => {
+  //   const fixture = TestBed.createComponent(AppComponent);
+  //   const app = fixture.componentInstance;
+  //   expect(app.title).toEqual('LakenVerHuur');
+  // });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
